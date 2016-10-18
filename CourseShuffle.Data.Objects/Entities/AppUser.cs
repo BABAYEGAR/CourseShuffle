eaa3e6607f25cf45adf48a18e05db4cedd0d342f
Haruna Salim <salxsaa@gmail.com>
2016-10-18 13:59:15 +0100

    Transport Object and Application Users object Created

---
+++ CourseShuffle.Data.Objects/Entities/AppUser.cs
@@ -0,0 +1,18 @@
+﻿namespace CourseShuffle.Data.Objects.Entities
+{
+    public class AppUser
+    {
+        public virtual string Firstname { get; set; }
+        public virtual string Lastname { get; set; }
+        public virtual string Othername { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string MatricNumber { get; set; }
+        public virtual string MobileNumber { get; set; }
+        public virtual string Level { get; set; }
+        public virtual string Role { get; set; }
+        public virtual Department Department { get; set; }
+        public virtual string DisplayName
+            => Firstname + " " + (string.IsNullOrEmpty(Othername) ? "" : Othername) + " " + Lastname;
+    }
+}
---
+++ CourseShuffle.Data.Objects/Entities/TransportObjects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseShuffle.Data.Objects.Entities
+{
+    public class TransportObjects
+    {
+        public virtual AppUser CreatedBy { get; set; }
+
+        public virtual DateTime DateCreated { get; set; }
+
+        public virtual DateTime DateLastModified { get; set; }
+
+        public virtual AppUser LastModifiedBy { get; set; }
+    }
+}
