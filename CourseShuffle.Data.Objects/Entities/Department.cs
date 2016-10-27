cd96cd92b5b1534084d982fe5addb2fc2e8335f9
usiju solomon <usijusolomon@gmail.com>
2016-10-28 00:10:08 +0100

    Created Category class for Projects and Subsciptiontype class

--- CourseShuffle.Data.Objects/Entities/Department.cs
+++ CourseShuffle.Data.Objects/Entities/Department.cs
@@ -24,5 +24,9 @@ namespace CourseShuffle.Data.Objects.Entities
         public IEnumerable<Courses> Courses { get; set; }
         public IEnumerable<AppUser> AppUsers { get; set; }
         public IEnumerable<Project> Projects { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+        public DateTime SubscriptionStartDate { get; set; }
+        public DateTime SubscriptionEndDate { get; set; }
+        public String SubscriptionType { get; set; }
     }
 }
